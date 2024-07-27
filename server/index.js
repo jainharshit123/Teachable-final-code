69992bc2da87526390b265d7b7030906471c0372
jainharshit <jainharshit112002@gmail.com>
2024-07-27 20:00:04 +0530

    new

--- server/index.js
+++ server/index.js
@@ -25,7 +25,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        // origin: "http://localhost:3000",
+        origin: "https://teachable-frontend.vercel.app",
         credentials: true,
     })
 )
